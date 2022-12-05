b2cf36c464bd4cc76a4a338a8c3e11a1bed2ac2b
kubabartecki <169511@stud.prz.edu.pl>
2022-12-05 08:43:58 +0100

    5 day

---
+++ 2022/5/5.py
@@ -0,0 +1,37 @@
+contain = 0
+elements = []
+
+with open('data.txt','r') as infile:
+    for lines in infile:
+        if lines[0] == " ": #end of input
+            break
+        elements.append(lines[1::4])#list of strings "W L THVFH" - form
+
+    stacks = [ [] for x in range(len(elements[0])) ] #initialize stack
+    for line in elements[::-1]:
+        for i, elem in enumerate(line):
+            stacks[i].append(elem)
+
+    #clean empty elements
+    for num in range(len(stacks)):
+        while (stacks[num].count(' ')):
+            stacks[num].remove(' ')
+        
+    print(stacks)
+
+    line = infile.readline()#just skip '\n'
+    
+    #operations
+    for line in infile:
+        #get digits from line
+        operation = [int(a) for a in line.strip('\n').split(' ') if a.isdigit()]
+
+        for num in range(operation[0]):
+            #operation on stacks
+            stacks[operation[2]-1].append(stacks[operation[1]-1].pop())
+    
+    result = ""
+    for stack in stacks:
+        result += str(stack[-1])
+        print(stack)
+    print(result)
