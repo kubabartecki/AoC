c08294fd472578015a02582b8bab2655d91887fd
kubabartecki <169511@stud.prz.edu.pl>
2022-12-06 13:48:09 +0100

    6 update

--- 2022/6/6.py
+++ 2022/6/6.py
@@ -1,18 +1,15 @@
 def part1and2(lines: str, digits: int) -> int:
-    result = digits - 1
-    
     for index in range(len(lines) - (digits - 1)):
         end = True
-        result += 1
         array = lines[index : index + digits]
-        print(array)
+        
         for elem in array:
             if array.count(elem) > 1:
                 end = False
                 break
         if end:
             break
-    return result
+    return index + digits
 
 
 with open('data.txt','r') as infile:
